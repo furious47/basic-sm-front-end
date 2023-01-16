64fdc7cd9bf43f8df31007cdbead6f1ba457ac39
furious47 <dhanushk370@gmail.com>
2023-01-16 12:05:20 +0530

    Error page err fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,6 @@ import DashBoard from "./Dashboard";
 import Register from "./Register";
 import PrivateRoute from "./PrivateRoute";
 import Edit from "./Edit";
-import Error from "./error";
+import Error from "./Error";
 
 export { DashBoard, Register, PrivateRoute, Edit, Error };
